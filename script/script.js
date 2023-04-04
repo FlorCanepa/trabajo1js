f76567684a316956271d5facb526cb52c8e03c6e
FlorCanepa <florenciacanepa97@gmail.com>
2023-04-03 22:21:14 -0300

    test

---
+++ script/script.js
@@ -0,0 +1,7 @@
+console.log("clase: ciclos e iteraciones");
+// ejemplo 1: estructura simple del ciclo FOR .
+let multiplicador  = parseInt(prompt(¿Con cuál número querés hacer la multiplicación?));
+for (let multiplicando = 0; multiplicando < array.length; multiplicando++) {
+    const element = array[multiplicando];
+    
+}
